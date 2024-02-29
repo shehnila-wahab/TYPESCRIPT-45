5593e3670fff2f0f706c2d86a5b49129ea3b8ac4
shehnila-wahab <160872560+shehnila-wahab@users.noreply.github.com>
2024-02-29 21:52:07 +0500

    EXERCISE 4

---
+++ app.js
@@ -0,0 +1,4 @@
+var quote = "a person who never made a mistake never tried anything new";
+var Famous_person = "Albert Einstein";
+var message = "".concat(Famous_person, " once said,").concat(quote);
+console.log(message);
