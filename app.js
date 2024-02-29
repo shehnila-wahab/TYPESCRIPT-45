54cb29c0ba09db66d1b492a5a6f0f08458b8f192
shehnila-wahab <160872560+shehnila-wahab@users.noreply.github.com>
2024-02-29 21:54:58 +0500

    EXERCISE 6

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var quote = "a person who never made a mistake never tried anything new";
-var Famous_person = "Albert Einstein";
-var message = "".concat(Famous_person, " once said,").concat(quote);
-console.log(message);
+var personName = "\n\t BABAR AZAM\t\n";
+console.log(personName);
+var stripped = personName.trim();
+console.log(stripped);
